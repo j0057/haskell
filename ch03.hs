8fd3e675081b943d9656993f8a0857c5b40224da
Joost Molenaar <j.j.molenaar@gmail.com>
2017-08-13 12:44:48 +0200

    Calculate two plus three

---
+++ ch03.hs
@@ -0,0 +1,24 @@
+#!/usr/bin/env runhaskell
+module Main (main) where
+
+import Test.HUnit
+
+add :: Int -> Int -> Int
+add a b = a + b
+
+fac :: Integer -> Integer
+fac n = foldr (*) 1 [1..n]
+
+fib :: Integer -> Integer
+fib 0 = 1
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
+main = do
+    runTestTT $ test [
+        "add tests" ~: test [ 5 ~=? (add 2 3),
+                              7 ~=? (add 3 4),
+                              9 ~=? (add 4 5) ],
+        "fac tests" ~: test [ 120 ~=? (fac 5),
+                              720 ~=? (fac 6) ],
+        "fib tests" ~: test [ 2 ~=? (fib 2) ] ]
