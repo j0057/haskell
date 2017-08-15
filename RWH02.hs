772c0af526b511a5e2b1c9de5a0f48c14f2d91a0
Joost Molenaar <j.j.molenaar@gmail.com>
2017-08-15 14:01:17 +0200

    RWH02: add secondLast function and tests

--- RWH02.hs
+++ RWH02.hs
@@ -6,7 +6,6 @@ module RWH02 (main) where
    and https://stackoverflow.com/revisions/6147930/2 ...
    and https://stackoverflow.com/a/6009807/424131 -}
 
--- import Control.Monad
 import Control.Exception
 import Test.HUnit
 
@@ -15,15 +14,37 @@ last' [] = error "last': empty list"
 last' (x:xs) | null xs   = x
              | otherwise = last' xs
 
+secondLast :: [a] -> a
+secondLast (x1:x2:xs) | null xs   = x1
+                      | otherwise = secondLast (x2:xs)
+secondLast (x:[]) = error "secondLast: list too short"
+secondLast []     = error "secondLast: empty list"
+
 main :: IO Counts
 main = do
     runTestTT $ test [
         "last tests" ~: test [
-            TestCase $ do { result <- try $ evaluate $ last' [] :: IO (Either ErrorCall Int)
-                          ; case result of
-                                Left ex -> return ()
-                                Right v -> assertFailure ("expected ErrorCall exception, not " ++ show v)
-                          },
+            test $ do { result <- try $ evaluate $ last' [] :: IO (Either ErrorCall Int)
+                      ; case result of
+                            Left ex -> return ()
+                            Right v -> assertFailure ("expected ErrorCall exception, not " ++ show v)
+                      },
             13 ~=? (last' [13]),
             13 ~=? (last' [1, 4, 13])
+        ],
+        "secondLast tests" ~: test [
+            test $ do { result <- try $ evaluate $ secondLast [] :: IO (Either ErrorCall Int)
+                      ; case result of
+                            Left ex -> return ()
+                            Right v -> assertFailure ("expected ErrorCall exception, not " ++ show v)
+                      },
+            test $ do { result <- try $ evaluate $ secondLast [1] :: IO (Either ErrorCall Int)
+                      ; case result of
+                            Left ex -> return ()
+                            Right v -> assertFailure ("expected ErrorCall exception, not " ++ show v)
+                      },
+            1 ~=? (secondLast [1, 2]),
+            2 ~=? (secondLast [1, 2, 3]),
+            3 ~=? (secondLast [1, 2, 3, 4]),
+            4 ~=? (secondLast [1, 2, 3, 4, 5])
         ] ]
